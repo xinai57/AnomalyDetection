2508e8c50e5e586788145738c06e9fd6a5f92098
Wang Qian <wangqiansishi@163.com>
2017-09-06 00:10:20 +0800

    if plot == FALSE, don't plot
    correct

--- R/ts_anom_detection.R
+++ R/ts_anom_detection.R
@@ -358,5 +358,6 @@ AnomalyDetectionTs <- function(x, max_anoms = 0.10, direction = 'pos',
   # Lastly, return anoms and optionally the plot if requested by the user
   if(plot){
     return (list(anoms = anoms, plot = xgraph))
-  }
+  } else {
+    return (list(anoms = anoms))
 }
