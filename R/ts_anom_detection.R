f91767175d5d9afc3f867bb4bab5381cba47a61d
Wang Qian <wangqiansishi@163.com>
2017-09-05 16:59:30 +0800

    if no anoms, don't plot
    lueluelue

--- R/ts_anom_detection.R
+++ R/ts_anom_detection.R
@@ -297,7 +297,7 @@ AnomalyDetectionTs <- function(x, max_anoms = 0.10, direction = 'pos',
   # If there are no anoms, then let's exit
   if(anom_pct == 0){
     if(verbose) message("No anomalies detected.")
-    return (list("anoms"=data.frame(), "plot"=plot.new()))
+    return (list("anoms"=data.frame()))
   }
 
   if(plot){
